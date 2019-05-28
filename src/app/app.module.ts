1b0b6e2ddb77ff5e412fb9162e5883b12220a4cc
Themba <thembangubeni04@gmail.com>
2019-05-28 00:02:46 -0700

    Added song service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
-//Angular Imports
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule }    from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { FormsModule }      from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule }     from './app-routing.module';
